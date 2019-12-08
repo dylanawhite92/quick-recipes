c8b93ed431445c7723c169b770648e55e9a0b183
The Crystal King <dylanawhite11@gmail.com>
2019-12-07 22:26:54 -0600

    set env variables for edamam keys

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const API_ID = process.env.REACT_APP_API_ID;
+  const API_KEY = process.env.REACT_APP_API_KEY;
+
+  console.log(API_ID);
+  console.log(API_KEY);
+
   return (
     <div className="App">
       <h1>Hello React</h1>
