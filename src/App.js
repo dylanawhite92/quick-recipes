cebf9429c68b07115863780f8f4f92814f1af9d8
The Crystal King <dylanawhite11@gmail.com>
2019-12-07 22:41:05 -0600

    implementing useState and useEffect hooks, just making sure they're working properly

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 const App = () => {
   const APP_ID = process.env.REACT_APP_API_ID;
   const APP_KEY = process.env.REACT_APP_API_KEY;
-
   const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
-
   console.log(exampleRequest);
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log("Use Effect is working");
+  })
 
   return (
     <div className="App">
       <form className="search-form">
         <input className="search-bar" type="text"/>
-        <button className="search-button" type="submit">Search</button>
+        <button  className="search-button" type="submit">
+          Search
+        </button>
       </form>
+
+      <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
     </div>
   );
 }
