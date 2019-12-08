f9977694a0aa517a4ab4fa2ed7b9c9a2723f3a57
The Crystal King <dylanawhite11@gmail.com>
2019-12-07 23:30:13 -0600

    handling search form submissions

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ const App = () => {
   const APP_ID = process.env.REACT_APP_API_ID;
   const APP_KEY = process.env.REACT_APP_API_KEY;
 
-  // State
+  // Default State Values
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken")
 
   // Query API for chicken recipes
   const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
     console.log(data.hits);
@@ -24,13 +25,19 @@ const App = () => {
     console.log(search);
   }
 
+  // Set the search query on submit
+  const getSearch = event => {
+    event.preventDefault();
+    setQuery(search);
+  }
+
   useEffect(() => {
     getRecipes();
   }, []);
 
   return (
     <div className="App">
-      <form className="search-form">
+      <form onSubmit={getSearch} className="search-form">
         <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
         <button  className="search-button" type="submit">
           Search
